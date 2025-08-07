0c7c1937475d64ffe2acd1f0a31cfa6106b584a1
Jose Storopoli <jose@storopoli.com>
2025-08-07 10:04:53 -0300

    feat: modernize flake

--- flake.nix
+++ flake.nix
@@ -1,39 +1,60 @@
 {
-  description = "";
+  description = "SP1 Nix Package Collection";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     sp1-src = {
       url = "github:succinctlabs/sp1";
       flake = false;
     };
-    systems.url = "github:nix-systems/default";
   };
 
   outputs =
-    inputs:
-    let
-      overlays = [ inputs.self.overlays.default ];
-      perSystemPkgs =
-        f:
-        inputs.nixpkgs.lib.genAttrs (import inputs.systems) (
-          system: f (import inputs.nixpkgs { inherit overlays system; })
-        );
-    in
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+      sp1-src,
+    }:
     {
       overlays = {
-        my-overlay = import ./overlay.nix { inherit (inputs) sp1-src; };
-        default = inputs.self.overlays.my-overlay;
+        my-overlay = import ./overlay.nix { inherit sp1-src; };
+        default = self.overlays.my-overlay;
       };
+    }
+    // flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            rust-overlay.overlays.default
+            self.overlays.default
+          ];
+        };
+      in
+      {
+        packages = {
+          cargo-prove = pkgs.cargo-prove;
+          sp1-rust-toolchain = pkgs.sp1-rust-toolchain;
+        };
 
-      packages = perSystemPkgs (pkgs: {
-        cargo-prove = pkgs.cargo-prove;
-        sp1-rust-toolchain = pkgs.sp1-rust-toolchain;
-      });
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo-prove
+            sp1-rust-toolchain
+          ];
+        };
 
-      devShells = perSystemPkgs (pkgs: { });
-
-      formatter = perSystemPkgs (pkgs: pkgs.nixfmt-rfc-style);
-    };
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
 }
